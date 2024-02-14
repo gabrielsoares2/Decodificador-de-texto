4acecd2a212757079c96b5caf152a9131332126f
Gabriel <soaresgabrielgsn@gmail.com>
2024-02-13 23:27:23 -0300

    teste

---
+++ script.js
@@ -0,0 +1,16 @@
+let cod = document.getElementById('entrada');
+let decod = document.getElementById('saida');
+
+function codificar() {
+
+    
+
+}
+
+function decodificar() {
+    
+}
+
+function copiar() {
+    
+}
